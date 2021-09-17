d7ec71933dd55ee78148c174ab8cfba91584a555
Abdul99-git <Abdul-Rahman-Kasim@hotmail.com>
2021-09-17 12:26:49 +0200

    Nice

--- Database.sql
+++ Database.sql
@@ -1,61 +1,91 @@
-DROP DATABASE Webshop;
+DROP DATABASE WEBSHOP;
+CREATE DATABASE WEBSHOP;
+use WEBSHOP;
 
-CREATE DATABASE Webshop;
-
-use Webshop;
-
-CREATE TABLE kunder (
-                        KundeID integer PRIMARY KEY NOT NULL ,
-                        Fornavn varchar (30) NOT NULL,
-                        Etternavn varchar (30) NOT NULL ,
-                        Passord varchar (60) NOT NULL ,
-                        Epost varchar (40) NOT NULL ,
-                        Telefonnummer integer (8) NOT NULL ,
-                        Adresse varchar (40) NOT NULL ,
-                        Postnummer integer (4) NOT NULL
-
+CREATE TABLE Customers (
+                           kundeId integer,
+                           Fornavn varchar (30),
+                           Etternavn varchar (30),
+                           Epost varchar (40),
+                           Tlfnummer int (10),
+                           Passord varchar (20),
+                           Adresse varchar (20),
+                           Postnummer int (5)
 
 );
+INSERT INTO Customers (kundeID, Fornavn, Etternavn, Epost, Tlfnummer, Passord, Adresse, Postnummer)
+values(1, 'Tom', 'Larsen', 'tomlarsen@hotmail.com', 41384997, 'tomlarsen123', 'solbakken 7', 4626);
+INSERT INTO Customers (kundeID, Fornavn, Etternavn, Epost, Tlfnummer, Passord, Adresse, Postnummer)
+values(2,'Petter', 'Jakobsen', 'petterjakobsen@hotmail.com', 98643913, 'petter123', 'åsane 12', 4020 );
+INSERT INTO Customers (kundeID, Fornavn, Etternavn, Epost, Tlfnummer, Passord, Adresse, Postnummer)
+values(3,'Jakob', 'Sørensen', 'jakobsørensen@hotmail.com', 48089121, 'jakob123', 'møvig 15', 4515 );
 
-INSERT INTO kunder (KundeID, Fornavn, Etternavn, Passord, Epost, Telefonnummer, Adresse, Postnummer)
-values (1,'Kristian','Larsen','adnasdjasudu22','kristianlarsen@gmail.com',92830112,'Bramstadveien 28',4626);
+update Customers
+SET Adresse='slettheiveien 15'
+where WEBSHOP.Customers.kundeID=3;
 
-INSERT INTO kunder (KundeID, Fornavn, Etternavn, Passord, Epost, Telefonnummer, Adresse, Postnummer)
-values (2,'Adam','Sin','sadasidsaid21','adamsin@gmail.com',92822112,'Hallaveien 18',4626);
+DELETE FROM Customers
+WHERE Fornavn='Tom';
 
-INSERT INTO kunder (KundeID, Fornavn, Etternavn, Passord, Epost, Telefonnummer, Adresse, Postnummer)
-values (3,'Bjørn','Olsen','iajsdiasjd17','bjornolsen@gmail.com',2819228,'Furuveien 28',4626);
+INSERT INTO Customers (kundeID, Fornavn, Etternavn, Epost, Tlfnummer, Passord, Adresse, Postnummer)
+values(4,'Mehmet', 'Eksi', 'MehmetEksi@hotmail,com', 41624571, 'memo123', 'Slettheiveien 66', 4626 );
 
-UPDATE kunder
-SET adresse='Mehmetveien'
-WHERE KundeID=1;
+INSERT INTO Customers (kundeID, Fornavn, Etternavn, Epost, Tlfnummer, Passord, Adresse, Postnummer)
+values(5,'Berat', 'Gunes', 'BeratGunes@hotmail.com', 97000949, 'berat', 'Tinnheiveien 15', 4208 );
 
-UPDATE kunder
-SET adresse='Abdulveien'
-WHERE KundeID=2;
 
-UPDATE kunder
-SET adresse='Omerveien'
-WHERE KundeID=3;
+SELECT kundeId, Fornavn, Epost FROM Customers;
 
-DELETE FROM kunder
-where Fornavn='Adam';
 
-INSERT INTO kunder (KundeID, Fornavn, Etternavn, Passord, Epost, Telefonnummer, Adresse, Postnummer)
-values (2, 'Mehmet', 'Eksi', 'babanisikem12', 'mehmeteksi@gmail.com', 48573991, 'Memo', 4626);
+DROP DATABASE Webshop;
 
-INSERT INTO kunder (KundeID, Fornavn, Etternavn, Passord, Epost, Telefonnummer, Adresse, Postnummer)
-values (4, 'Abdul', 'Kasim', 'pakistani211', 'abdulkasim@gmail.com', 2847719, 'pakistaniwarrior', 4626);
+CREATE DATABASE WEBSHOP;
+use WEBSHOP;
 
-SELECT KundeID, Fornavn, Epost FROM kunder;
+CREATE TABLE Customers (
+                           kundeID integer PRIMARY KEY NOT NULL,
+                           Fornavn varchar (30) NOT NULL,
+                           Etternavn varchar (30) NOT NULL,
+                           Epost varchar (40) NOT NULL,
+                           Telefonnummer integer (8) NOT NULL,
+                           Passord varchar (20) NOT NULL,
+                           Adresse varchar (20) NOT NULL,
+                           Postnummer int (4) NOT NULL
+
+);
+INSERT INTO Customers (kundeID, Fornavn, Etternavn, Epost, Telefonnummer, Passord, Adresse, Postnummer)
+values(1, 'Tom', 'Larsen', 'tomlarsen@hotmail.com', 41384997, 'tomlarsen123', 'solbakken 7', 4626);
+INSERT INTO Customers (kundeID, Fornavn, Etternavn, Epost, Telefonnummer, Passord, Adresse, Postnummer)
+values(2,'Petter', 'Jakobsen', 'petterjakobsen@hotmail.com', 98643913, 'petter123', 'åsane 12', 4020 );
+INSERT INTO Customers (kundeID, Fornavn, Etternavn, Epost, Telefonnummer, Passord, Adresse, Postnummer)
+values(3,'Jakob', 'Sørensen', 'jakobsørensen@hotmail.com', 48089121, 'jakob123', 'møvig 15', 4515 );
+
+update Customers
+SET Adresse='Tranestien 9'
+WHERE kundeID=3;
+
+DELETE FROM Customers
+WHERE Fornavn='Tom';
+
+INSERT INTO Customers (kundeID, Fornavn, Etternavn, Epost, Telefonnummer, Passord, Adresse, Postnummer)
+values(4,'Mehmet', 'Eksi', 'MehmetEksi@hotmail,com', 41624571, 'memo123', 'Slettheiveien 66', 4626 );
+
+INSERT INTO Customers (kundeID, Fornavn, Etternavn, Epost, Telefonnummer, Passord, Adresse, Postnummer)
+values(5,'Berat', 'Gunes', 'BeratGunes@hotmail.com', 41654751, 'berat', 'Tinnheiveien 15', 4208 );
+
+
+
+SELECT kundeID, Fornavn, Epost FROM Customers;
 
 CREATE TABLE ordre (
                        OrderID integer PRIMARY KEY NOT NULL,
-                       KundeID integer NOT NULL ,
-                       OrdreDato date NOT NULL ,
-                       Produkter varchar (50) ,
-                       FOREIGN KEY (KundeID) REFERENCES kunder(KundeID)
+                       kundeID integer NOT NULL ,
+                       OrdreDato date NOT NULL,
+                       Produkter varchar (50),
+                       FOREIGN KEY (KundeID) REFERENCES Customers(KundeID)
+
+
 );
 
-INSERT INTO ordre (OrderID, KundeID, OrdreDato, Produkter)
-values (1, 2, '2011-11-11', 'Fatimaa sin nesehårsfjerner');
+INSERT INTO ordre (OrderID, kundeID, OrdreDato, Produkter)
+values(1, 4, '2011-11-11', 'memo');
