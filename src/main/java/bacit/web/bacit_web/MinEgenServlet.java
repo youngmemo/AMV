badfcc191d4c8b21c9043648889635581da136d6
Abdul99-git <Abdul-Rahman-Kasim@hotmail.com>
2021-10-07 01:15:09 +0200

    Espen 202

--- src/main/java/bacit/web/bacit_web/MinEgenServlet.java
+++ src/main/java/bacit/web/bacit_web/MinEgenServlet.java
@@ -13,31 +13,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
-@WebServlet(name = "GetUserServlet", value = "/GetUserServlet")
-public class GetUserServlet extends HttpServlet {
+@WebServlet(name = "MinEgenServlet", value = "/MinEgenServlet")
+public class MinEgenServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
-        String uname = request.getParameter("uname");
+            throws ServletException, IOException {
+        String lastName = request.getParameter("lastName");
         PrintWriter out = response.getWriter();
         try {
-            UserModel model = getUser(uname, out);
+            UserModel model = getUser(lastName, out);
 
-            out.println(model.getFirstName());
+            out.println(model.getLastName());
+            out.println("Denne brukeren fantes i databasen!");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
 
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
 
     }
 
-    private UserModel getUser(String uname, PrintWriter out) throws SQLException {
+    private UserModel getUser(String lastName, PrintWriter out) throws SQLException {
         Connection db = null;
         try {
             db = DBUtils.getINSTANCE().getConnection(out);
@@ -45,16 +45,19 @@ public class GetUserServlet extends HttpServlet {
             e.printStackTrace();
         }
 
-        String query3 = "select * from user where User_firstName = ?";
-        PreparedStatement statement = db.prepareStatement(query3);
-        statement.setString(1, uname);
+        String etternavnQuery = "select * from user where User_lastName = ?";
+        PreparedStatement statement = db.prepareStatement(etternavnQuery);
+        statement.setString(1, lastName);
         ResultSet rs = statement.executeQuery();
         UserModel model = null;
         while (rs.next()) {
             model =
-                new UserModel(rs.getString("User_firstName"), rs.getString("User_lastName"), rs.getString("User_Email"),
-                    rs.getString("User_password"), rs.getString("User_dob"));
+                    new UserModel(rs.getString("User_firstName"), rs.getString("User_lastName"), rs.getString("User_Email"),
+                            rs.getString("User_password"), rs.getString("User_dob"));
         }
         return model;
     }
 }
+
+
+
