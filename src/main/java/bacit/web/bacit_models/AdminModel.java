1e808c38f8e896de36d103a3e600425f3a70bf52
beratgunes <beratg12@hotmail.com>
2021-10-15 16:14:05 +0200

    la til adminmodel, giadminservlet

---
+++ src/main/java/bacit/web/bacit_models/AdminModel.java
@@ -0,0 +1,13 @@
+package bacit.web.bacit_models;
+
+public class AdminModel {
+    private String AnsattID;
+
+    public String getAnsattID() {
+        return AnsattID;
+    }
+
+    public void setAnsattID(String ansattID) {
+        this.AnsattID = ansattID;
+    }
+}
