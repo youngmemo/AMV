5cc450f641f0fdf2fff57fce3a094ae39bcba763
beratgunes <beratg12@hotmail.com>
2021-10-18 22:03:29 +0200

    giadminservlet forbedret

--- src/main/java/bacit/web/bacit_models/AdminModel.java
+++ src/main/java/bacit/web/bacit_models/AdminModel.java
@@ -2,6 +2,7 @@ package bacit.web.bacit_models;
 
 public class AdminModel {
     private String AnsattID;
+    private String kommentar;
 
     public String getAnsattID() {
         return AnsattID;
@@ -10,4 +11,8 @@ public class AdminModel {
     public void setAnsattID(String ansattID) {
         this.AnsattID = ansattID;
     }
+
+    public String getKommentar() { return kommentar; }
+
+    public void setKommentar(String kommentar){ this.kommentar = kommentar; }
 }
