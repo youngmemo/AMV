cffb716c13355ee3f9693efd34a81e0dd9097061
Mehmet Eksi <mehmeteksi99@hotmail.com>
2021-10-22 11:06:25 +0200

    La inn espen sine nye filer for uploads osv

--- src/main/java/bacit/web/bacit_utilities/HtmlHelper.java
+++ src/main/java/bacit/web/bacit_utilities/HtmlHelper.java
@@ -45,4 +45,20 @@ public class HtmlHelper {
         out.println("</body>");
         out.println("</html>");
     }
+
+    public static void writeHtmlStartEspen(PrintWriter out, String title) {
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>"+title+"</title>");
+
+        out.println("<!-- Latest compiled and minified CSS -->\n" +
+                "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\" integrity=\"sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu\" crossorigin=\"anonymous\">\n" +
+                "\n" +
+                "<!-- Optional theme -->\n" +
+                "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css\" integrity=\"sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ\" crossorigin=\"anonymous\">");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h2>"+title+"</h2>");
+    }
+
 }
