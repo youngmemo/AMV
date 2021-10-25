f8ff51b76dcd65e56cc5c9ce6172b17cc325289e
beratgunes <beratg12@hotmail.com>
2021-10-25 14:18:16 +0200

    bekreftt.levering gjort ferdig, testet fint.

--- src/main/java/bacit/web/bacit_web/BekreftTilbakeleveringServlet.java
+++ src/main/java/bacit/web/bacit_web/BekreftTilbakeleveringServlet.java
@@ -30,9 +30,8 @@ import java.sql.SQLException;
                 BekreftTilbakeleveringModel model = new BekreftTilbakeleveringModel();
                 HtmlHelper HtmlHelper = new HtmlHelper();
 
-                model.setAnsatt_ID(request.getParameter("Ansattnummeret"));
                 model.setForesporsel_ID(request.getParameter("Statusen"));
-                model.setUtsyr_ID(request.getParameter("Utstyret"));
+
 
                 PrintWriter out = response.getWriter();
 
@@ -43,11 +42,11 @@ import java.sql.SQLException;
                         out.println(ex.getMessage());
                     }
                     HtmlHelper.writeHtmlStart(out, "Tilbakeleveringen er bekreftet!");
-                    out.println("<br><b>Ansattnummeret:</b> " + model.getAnsatt_ID());
+
                     out.println("<br><b>Statusen:</b>" + model.getForesporsel_ID());
-                    out.println("<br><b>Utstyret:</b>" + model.getUtsyr_ID());
+
                     HtmlHelper.writeHtmlEnd(out);
-                    ;
+
                 } else {
                     BekreftsProsessInput(out, "Ops! Det skjedde noe feil..");
                 }
@@ -57,14 +56,9 @@ import java.sql.SQLException;
                 Connection db = null;
                 try {
                     db = DBUtils.getINSTANCE().getConnection(out);
-                    String leggeTilKode = "select Utstyr_ID, Start_Dato, Slutt_Dato from Foresporsel " +
-                            "inner join Foresporsel on Utstyr.Foresporsel_ID = Foresporsel.Status_ID " +
-                            "where Ansatt_ID = (?,?,?);";
+                    String leggeTilKode = "update Status set Levert = true where Foresporsel_ID = ?;";
                     PreparedStatement kode = db.prepareStatement(leggeTilKode);
-                    kode.setString(1, model.getAnsatt_ID());
-                    kode.setString(2, model.getForesporsel_ID());
-                    kode.setString(3, model.getUtsyr_ID());
-
+                    kode.setString(1, model.getForesporsel_ID());
                     kode.executeUpdate();
 
                 } catch (ClassNotFoundException e) {
@@ -81,16 +75,13 @@ import java.sql.SQLException;
                     }
                     out.println("<form action='/bacit-web-1.0-SNAPSHOT/ansatt/tilbake-levering' method='POST'>");
 
-                    out.println("<br><b> <label for='Ansattnummeret'> Ansattnummeret</label>");
-                    out.println("<input type='text' name= 'Ansattnummeret' placeholder='Skriv inn navnet'/>");
+
 
                     out.println("<br><b> <label for='Statusen'> Statusen</label>");
                     out.println("<input type= 'text' name= 'Statusen' placeholder='Skriv inn statusen'/>");
 
-                    out.println("<br><b> <label for='Utstyret'> Utstyret</label>");
-                    out.println("<input type= 'text' name= 'Utstyret' placeholder= 'Skriv inn utstyret'/>");
 
-                    out.println("<br><br> <input type='submit' value='Lever'/>");
+                    out.println("<br><br> <input type='submit' value='Levert'/>");
                     out.println("</form>");
 
                     HtmlHelper.writeHtmlEnd(out);
@@ -109,15 +100,10 @@ import java.sql.SQLException;
                      out.println("</html>");
                  }
                 private boolean CheckBekreftTilbakelevering(BekreftTilbakeleveringModel model){
-                    if(model.getAnsatt_ID() == null)
-                        return false;
-                    if(model.getAnsatt_ID().trim().equalsIgnoreCase(""))
-                        return false;
+
                     if(model.getForesporsel_ID() == null)
                         return false;
                     if(model.getForesporsel_ID().trim().equalsIgnoreCase(""))
-                        return false;
-                    if(model.getUtsyr_ID().trim().equalsIgnoreCase(""))
                         return false;
 
                     return true;
